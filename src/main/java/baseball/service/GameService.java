5ba23f2f0f56558842294f91f34c1fd05ba0bc06
cyr9210 <jipsyer@naver.com>
2022-04-13 21:25:04 +0900

    feat: 생성한 숫자에 입력한 숫자가 포함한 갯수를 확인한다.

---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,11 @@
+package baseball.service;
+
+import baseball.domain.Numbers;
+
+public class GameService {
+
+    public void check(Numbers answer, Numbers inputNumbers) {
+        int containsCount = answer.getContainsCount(inputNumbers);
+
+    }
+}
--- src/test/java/baseball/domain/NumbersTest.java
+++ src/test/java/baseball/domain/NumbersTest.java
@@ -8,21 +8,23 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class NumbersTest {
 
     @DisplayName("서로 다른 1~9 사이의 숫자 3자리를 생성한다.")
     @Test
     public void generateNumbers() {
-        Numbers numbers = Numbers.generate(this::createNumberMock);
+        Numbers numbers = Numbers.generate(() -> createNumberMock(1, 2, 3));
         assertThat(numbers.getNumbers()).containsExactly(new Number(1), new Number(2), new Number(3));
     }
 
-    private List<Integer> createNumberMock() {
+    private List<Integer> createNumberMock(int first, int second, int third) {
         List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
+        numbers.add(first);
+        numbers.add(second);
+        numbers.add(third);
         return numbers;
     }
 
@@ -39,10 +41,20 @@ class NumbersTest {
     @DisplayName("숫자가 4자리 이상인 경우, IllegalArgumentException 발생")
     @Test
     public void generateNumbers_fail() {
-        List<Integer> numberMock = createNumberMock();
+        List<Integer> numberMock = createNumberMock(1, 2, 3);
         numberMock.add(4);
 
         assertThatIllegalArgumentException().isThrownBy(() -> Numbers.generate(() -> numberMock));
+    }
+
+    @DisplayName("포함된 숫자의 갯수를 구한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"3,1,2,3", "4,1,2,2", "4,5,2,1", "4,5,6,0"})
+    public void getContainsCount(int inputFirst, int inputSecond, int inputThird, int result) {
+        Numbers answer = Numbers.generate(() -> createNumberMock(1, 2, 3));
+        Numbers inputNumbers = Numbers.generate(() -> createNumberMock(inputFirst, inputSecond, inputThird));
+        int containsCount = answer.getContainsCount(inputNumbers);
+        assertEquals(result, containsCount);
     }
 
 }
